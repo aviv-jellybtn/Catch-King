e2b6706bd4a2fa09ae63da746581986df3c14cbc
Aviv Ezra <aviv.e@jellybtn.com>
2016-12-10 12:38:20 +0200

    - auto position borders

---
+++ Assets/Catch-King/Scripts/Components/Border.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Border : MonoBehaviour
+{
+    [SerializeField] private bool _isRight;
+
+    private void Awake()
+    {
+        PositionBorder(_isRight);
+    }
+
+    private void PositionBorder(bool isRight)
+    {
+        var stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0));
+
+        if (_isRight)
+        {
+            transform.localPosition = new Vector3(stageDimensions.x +0.5f, -2, 0);
+        }
+        else
+        {
+            transform.localPosition = new Vector3(-stageDimensions.x - 0.5f, -2, 0);
+
+        }
+    }
+
+}
